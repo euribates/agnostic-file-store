dc582cc2847649cb00aa3b19546a3d956012d760
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2021-03-07 14:49:10 +0000

    Change repo from bitbucket to github

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ from distutils.core import setup
 setup(
     name='agnostic-file-store',
     packages=['afs'],
-    version='0.2.0',
+    version='0.3.1',
     description='An agnostic, easy-to-use module for different file systems'
                   ' (At present just local an SMB)',
     author='Juan Ignacio Rodríguez de León',
     author_email='euribates@gmail.com',
     url='https://github.com/euribates/agnostic-file-store/',
     download_url='https://github.com/euribates/agnostic-file-store/archive/master.zip',
+    long_description='README.md',
+    long_description_content_type='markdown',
     keywords=['agnostic', 'SAN', 'NAS', 'Samba', 'SMB', 'CIFS'],
     classifiers= [
         'Development Status :: 4 - Beta',
